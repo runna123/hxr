137c9a95a371f919a0a9d32de2150a6151f48268
runna <2539802494@qq.com>
2019-09-23 20:31:54 +0800

    created a new Age

---
+++ Age.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Age {
+	public static void main (String[] args){
+		final String younger = "cute angel";
+		final String young = "beautiful angel";
+		final String jiejie = "xu~";
+		Scanner scan = new Scanner(System.in);
+		System.out.print("pleas input your age: ");
+		int age = scan.nextInt();
+		
+		String nickname = jiejie;
+		if(age<20){
+			nickname = younger;
+		}
+		if(age>=20){
+			nickname = young;
+		}
+		switch(nickname){
+			case younger:
+			    System.out.print("hello cute angel");
+				break;
+			case young:
+				System.out.print("hello beautiful angel");
+				break;
+			default:
+			    System.out.print("ples input again");
+		}
+	}
+}
